2c9b3e0acc08899dd9afc7f470b89224a6f2cb21
sergey <malsn@rambler.ru>
2018-10-07 20:46:05 +0300

    test pdf output

--- lib/pdf/PDF.php
+++ lib/pdf/PDF.php
@@ -14,7 +14,7 @@ class PDF extends FPDF
         // Move to the right
         $this->Cell(80);
         // Framed title
-        $this->Cell(30,10,'Title',1,0,'C');
+        $this->Cell(30,10,'Поручение на перевозку груза',0,0,'C');
         // Line break
         $this->Ln(20);
     }
